a8b0ad3f433684a2d8a7aa5cb01931939f15e143
cemgthedev <cemg.the.dev@gmail.com>
2024-12-29 15:13:40 -0300

    feat: implementação da rota de criação de exercício

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from sqlmodel import Session
 from database import get_db
 from models import *
 from services.users import router as users_router
+from services.exercices import router as exercices_router
 
 app = FastAPI()
 
@@ -12,4 +13,8 @@ def get_db(db: Session = Depends(get_db)):
         return {"message": "Database not connected"}
     return {"message": "Database connected"}
 
+# Adicionando rotas de usuários
 app.include_router(users_router)
+
+# Adicionando rotas de exercícios
+app.include_router(exercices_router)
---
+++ src/services/exercices.py
@@ -0,0 +1,20 @@
+# Criar roteador
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+
+from database import get_db
+from models.exercice import Exercice
+
+# Criar roteador
+router = APIRouter()
+
+# Rota para criar um novo exercício
+@router.post("/exercices")
+async def create_user(exercice: Exercice, db: Session = Depends(get_db)):
+    try:
+        db.add(exercice)
+        db.commit()
+        return {"message": "Exercice created successfully"}
+    except Exception as e:
+        db.rollback()
+        return {"error": str(e)}
