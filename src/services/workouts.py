d969f19bf8d7cba76522dad817730dd065e2615c
cemgthedev <cemg.the.dev@gmail.com>
2024-12-29 16:27:45 -0300

    feat: implementando rota de get de treino

--- src/services/workouts.py
+++ src/services/workouts.py
@@ -1,6 +1,6 @@
 # Criar roteador
 from fastapi import APIRouter, Depends
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from database import get_db
 from models.workout import Workout
@@ -18,4 +18,15 @@ async def create_workout(workout: Workout, db: Session = Depends(get_db)):
         return {"message": "Workout created successfully", "data": workout}
     except Exception as e:
         db.rollback()
+        return {"error": str(e)}
+
+# Rota para pegar treino pelo id
+@router.get("/workouts/{id}")
+async def get_workout(id: int, db: Session = Depends(get_db)):
+    try:
+        workout = db.exec(select(Workout).where(Workout.id == id)).first()
+        if workout is None:
+            return {"error": "Workout not found"}
+        return {"message": "Workout found successfully", "data": workout}
+    except Exception as e:
         return {"error": str(e)}
