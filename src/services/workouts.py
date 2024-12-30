aacc47ab4167ac7a416407d1ba76b52cd0563a18
cemgthedev <cemg.the.dev@gmail.com>
2024-12-30 17:27:58 -0300

    feat: implementação de logs nas rotas de treinos

--- src/services/workouts.py
+++ src/services/workouts.py
@@ -6,6 +6,7 @@ from sqlmodel import Session, and_, select
 from sqlalchemy.sql import func
 from database import get_db
 from models.workout import Workout
+from services.configs import workouts_logger as logger
 
 # Criar roteador
 router = APIRouter()
@@ -14,20 +15,26 @@ router = APIRouter()
 @router.post("/workouts")
 async def create_workout(workout: Workout, db: Session = Depends(get_db)):
     try:
+        logger.info(f"Criando um novo treino...")
         db.add(workout)
         db.commit()
         db.refresh(workout)
+        
+        logger.info(f"Treino criado com sucesso!")
         return {"message": "Workout created successfully", "data": workout}
     except Exception as e:
         db.rollback()
+        logger.error(f"Erro ao criar um novo treino: {str(e)}")
         return {"error": str(e)}
     
 # Rota para atualizar um treino
 @router.put("/workouts/{id}")
 async def update_workout(id: int, updated_workout: Workout, db: Session = Depends(get_db)):
     try:
+        logger.info(f"Atualizando treino com ID: {id}")
         workout = db.exec(select(Workout).where(Workout.id == id)).first()
         if workout is None:
+            logger.warning(f"Treino com ID {id} nao encontrado")
             return {"error": "Workout not found"}
         workout.title = updated_workout.title
         workout.description = updated_workout.description
@@ -37,6 +44,8 @@ async def update_workout(id: int, updated_workout: Workout, db: Session = Depend
         
         db.commit()
         db.refresh(workout)
+        
+        logger.info(f"Treino atualizado com sucesso!")
         return {"message": "Workout updated successfully", "data": workout}
     except Exception as e:
         db.rollback()
@@ -46,24 +55,33 @@ async def update_workout(id: int, updated_workout: Workout, db: Session = Depend
 @router.delete("/workouts/{id}")
 async def delete_workout(id: int, db: Session = Depends(get_db)):
     try:
+        logger.info(f"Removendo treino com ID: {id}")
         workout = db.exec(select(Workout).where(Workout.id == id)).first()
         if workout is None:
+            logger.warning(f"Treino com ID {id} nao encontrado")
             return {"error": "Workout not found"}
         
         db.delete(workout)
         db.commit()
+        
+        logger.info(f"Treino removido com sucesso!")
         return {"message": "Workout deleted successfully"}
     except Exception as e:
         db.rollback()
+        logger.error(f"Erro ao remover um treino: {str(e)}")
         return {"error": str(e)}
 
 # Rota para pegar treino pelo id
 @router.get("/workouts/{id}")
 async def get_workout(id: int, db: Session = Depends(get_db)):
     try:
+        logger.info(f"Buscando treino com ID: {id}")
         workout = db.exec(select(Workout).where(Workout.id == id)).first()
         if workout is None:
+            logger.warning(f"Treino com ID {id} nao encontrado")
             return {"error": "Workout not found"}
+        
+        logger.info(f"Treino encontrado: {workout}")
         return {"message": "Workout found successfully", "data": workout}
     except Exception as e:
         return {"error": str(e)}
@@ -82,6 +100,7 @@ async def get_workouts(
     category: Optional[str] = Query(None, description="Filter by workout category")
 ):
     try:
+        logger.info(f"Buscando treinos...")
         filters = []
         if title:
             filters.append(Workout.title.ilike(f"%{title}%"))
@@ -102,6 +121,11 @@ async def get_workouts(
 
         total_workouts = db.exec(select(func.count()).select_from(Workout).where(and_(*filters))).first() if filters else db.exec(select(func.count()).select_from(Workout)).first()
         total_pages = ceil(total_workouts / limit)
+        
+        if total_workouts > 0:
+            logger.info(f"Encontrados {total_workouts} treinos")
+        else:
+            logger.info(f"Nenhum treino encontrado")
 
         return {
             "message": "Workouts found successfully",
@@ -112,13 +136,18 @@ async def get_workouts(
             "total_pages": total_pages
         }
     except Exception as e:
+        logger.error(f"Erro ao buscar treinos: {str(e)}")
         return {"error": str(e)}
     
 # Rota para retorno da quantidade de treinos
 @router.get("/quantity/workouts")
 async def get_workouts_quantity(db: Session = Depends(get_db)):
     try:
+        logger.info(f"Calculando quantidade de treinos...")
         quantity = db.exec(select(func.count()).select_from(Workout)).first()
+        
+        logger.info(f"Quantidade de treinos: {quantity}")
         return {"message": "Workouts quantity found successfully", "data": str(quantity)}
     except Exception as e:
+        logger.error(f"Erro ao calcular a quantidade de treinos: {str(e)}")
         return {"error": str(e)}
